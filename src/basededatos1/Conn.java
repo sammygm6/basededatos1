16ed510ed8b642246849ef7055a655d7f5a84fe3
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2017-03-14 21:43:06 -0600

    Agregando la clase para la coneccion a la bd y el .jar para ver bien el gui

---
+++ src/basededatos1/Conn.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basededatos1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Miguel A. Ardon E
+ */
+public class Conn {
+    //no se les olvide que siempre hay que poner nuestro usurio y nuestar contraseña
+    String IP;
+     String Puerto;
+     String BD;
+     String Usuario;
+     String Clave;
+     public Connection conn;
+     public Conn(){
+          this.IP="";
+          this.Puerto="";
+          this.BD="";
+          this.Usuario="";
+          this.Clave=""; 
+          this.conn = null;
+     }
+     public Connection mkConnRe() throws Exception {
+                try{
+                  /*Datos de la BD*/
+                  this.IP      = "localhost";
+                  this.Puerto  = "3306";
+                  this.BD      = "transportecarmen";
+                  this.Usuario = "";
+                  this.Clave   = "";
+                  String driver = "com.mysql.jdbc.Driver";
+                  String url = "jdbc:mysql://"+this.IP+":"+this.Puerto+"/"+this.BD+
+                          "?noAccessToProcedureBodies=true&amp;zeroDateTimeBehavior=convertToNull";  
+                  Class.forName(driver).newInstance();
+		  this.conn = DriverManager.getConnection(url,this.Usuario,this.Clave);
+                }catch(Exception e){
+                  e.printStackTrace();
+                }                                
+           return this.conn;
+        }
+}
